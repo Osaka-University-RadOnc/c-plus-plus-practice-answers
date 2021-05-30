bd391134b26dd01f6d89c2b7a82158ef53909f79
doikentaro <61648865+doiken1130@users.noreply.github.com>
2021-05-31 00:49:34 +0900

    Create cplpl_beginner.cpp

---
+++ cplpl_beginner.cpp
@@ -0,0 +1,20 @@
+// 初級編 part 20~
+
+// part 20 //
+#include <iostream>
+using namespace std;
+int main(){
+    int ia;
+    int iia;
+    int x;
+    int i = 0;
+    cout << "iput your 1st number \n";
+    cin >> ia;
+    cout << "iput your 2nd number \n";
+    cin >> iia;
+    cout << "1st number is " << ia << ".\n" << "2nd number is " << iia << ".\n" << endl;
+    x = ia/iia;
+    cout << "result: " << x << "\n" << "result: " << x*iia << endl;
+
+    return 0;
+}
